79500d66dac3bca6d7d0e0d7bdafaf69a8ac9fb6
pengwei <mr.pengwei@outlook.com>
2021-01-23 11:33:35 +0800

    new

--- tasklist/app.js
+++ tasklist/app.js
@@ -5,13 +5,19 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Call all event listeners
+// Load all event listeners
 loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filster tasks event
+    filter.addEventListener('keyup', filterTasks); //键盘抬起时触发
 }
 
 // Add Task
@@ -38,8 +44,59 @@ function addTask(e) {
     // Append li to ul
     taskList.appendChild(li);
 
+    // Store in Local Storage
+    storeTasks(taskInput.value);
+
     // Clear input
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Store Tasks in Local
+function storeTasks(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    // Faster
+    while(taskList.firstChild) {            
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach //querySelectorAll返回一个nodelist
+    (function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !== -1) { //若不存在firstChild则index为-1
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
