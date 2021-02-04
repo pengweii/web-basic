e677dfd35fb8d41dc2142d5d2d4452b932e6ed1a
pengwei <mr.pengwei@outlook.com>
2021-02-04 22:48:29 +0800

    new

--- The Complete JavaScript Course From Zero to Expert/10-Closer-look-at-Functions/script.js
+++ The Complete JavaScript Course From Zero to Expert/10-Closer-look-at-Functions/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const bookings = [];
+
+function createBooking(
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 默认值
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    // flightNum: flightNum;
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123', 2);
+createBooking('LH123', undefined, 100); // 跳过某个值不能和array一样空着，要设为undefined
